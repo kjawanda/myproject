7537334a9f07288b89bddc90f64426f743aa71b7
kjawanda <kjawanda@hotmail.co.uk>
2018-03-06 22:31:30 +0000

    second commit

--- alignment.py
+++ alignment.py
@@ -2,11 +2,18 @@ from Bio import SwissProt
 from Bio import SeqIO
 from Bio import AlignIO
 from Bio import Phylo
+from Bio import pairwise2
+import gzip
 import io
 import subprocess
+import sys
+from Bio.pairwise2 import format_alignment
 from Bio.Align.Applications import ClustalwCommandline
 from Bio.Align.Applications import MuscleCommandline
-import gzip
+from Bio.Align.Applications import TCoffeeCommandline
+from Bio.Emboss.Applications import NeedleCommandline
+from Bio.Emboss.Applications import WaterCommandline
+from Bio.Emboss.Applications import NeedleallCommandline
 
 handle = gzip.open("uniprot_sprot.xml.gz")
 records = SeqIO.parse(handle, "uniprot-xml")
@@ -26,7 +33,7 @@ align = AlignIO.read("opuntia.aln","clustal")
 print(align)
 tree = Phylo.read("opuntia.dnd", "newick")
 Phylo.draw_ascii(tree)
-"""
+
 print("MUSCLE Alignment:")
 print("Muscle with a small input:")
 muscle_cline = MuscleCommandline(input="opuntia.fasta")
@@ -34,9 +41,9 @@ stdout, stderr = muscle_cline()
 muscle_align = AlignIO.read(io.StringIO(stdout), "fasta")
 print(muscle_align)
 
-"""The approach used above is simple but if you are dealing with very large output text the fact that all of stdout and
+The approach used above is simple but if you are dealing with very large output text the fact that all of stdout and
 stderr is loaded into memory as a string can be a potential drawback.
-Using the 'subprocess' module we can work directly with handles instead: """
+Using the 'subprocess' module we can work directly with handles instead: 
 
 print("Muscle with a large input:")
 
@@ -45,9 +52,89 @@ child = subprocess.Popen(str(muscle_cline),
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE,
                          universal_newlines=True,
-                         shell=(sys.platform!="win32"))
+                         shell=(sys.platform!="linux2"))
 muscle_align = AlignIO.read(child.stdout, "fasta")
 print(muscle_align)
-#name sys is not defined. ask jen
+
+print("T-Coffee alignment:")
+tcoffee_cline = TCoffeeCommandline(infile="opuntia.fasta",
+                                   output="clustalw",
+                                   outfile="tcoffee_aligned.aln")
+
+tcoffee_aligned = AlignIO.read("tcoffee_aligned.aln", "clustal")
+print(align)
+
+
+
+
+
+child = subprocess.Popen(str(tcoffee_cline),
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         universal_newlines=True,
+                         shell=(sys.platform!="linux2"))
+tcoffee_align = AlignIO.read(child.stdout, "fasta")
+print(tcoffee_align)
+"""
+
+
+print("pairwise global alignment:")
+X = "ACGGGT"
+Y = "ACG"
+alignments = pairwise2.align.globalms(X, Y, 2, -1, -0.5, -0.1)
+for a in alignments:
+    print(format_alignment(*a))
+
+
+print("pairwise local alignment:")
+X = "ACGGGT"
+Y = "ACG"
+alignments = pairwise2.align.localms(X, Y, 2, -1, -0.5, -0.1)
+for a in alignments:
+    print(format_alignment(*a))
+
+print("pairwise alignments using EMBOSS")
+print("pairwise with needle")
+needle_cline = NeedleCommandline(asequence = "alpha.faa", bsequence = "beta.faa",
+                                 gapopen=10, gapextend=0.5, outfile="needle.txt")
+print(needle_cline)
+stdout, stderr = needle_cline()
+print(stdout + stderr)
+
+needle_align = AlignIO.read("needle.txt", "emboss")
+print(needle_align)
+
+
+print("pairwise with water")      
+water_cline = WaterCommandline(asequence = "alpha.faa", bsequence = "beta.faa",
+                                 gapopen=10, gapextend=0.5, outfile="water.txt")
+print(water_cline)
+stdout, stderr = water_cline()
+print(stdout + stderr)
+
+water_align = AlignIO.read("water.txt", "emboss")
+print(water_align)
+
+print("this is the name of the script:", sys.argv[1])
+
+
+"""
+print("pairwise where many sequences are used, with needle")
+many_needle_cline = NeedleallCommandline(asequence = "alpha.faa",bsequence = "gamma.faa",gapopen=10, gapextend=0.5, outfile="many_needle.txt")
+print(many_needle_cline)
+
+stdout, stderr = many_needle_cline()
+print(stdout + stderr)
+
+many_needle_align = AlignIO.read("many_needle.txt", "emboss")
+print(many_needle_align)
+
+#https://www.biostars.org/p/237209/
+#http://emboss.sourceforge.net/apps/release/6.2/emboss/apps/needleall.html
+#doesnt do many-many pairwise, only one pairwise alignment
+#http://emboss.sourceforge.net/apps/release/6.6/emboss/apps/needleall.html
+
+"""
+
 
 
